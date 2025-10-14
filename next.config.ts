45e241b4be0c6ab066729fb4f36f7444c74466be
jgtourao <166062263+jgtourao@users.noreply.github.com>
2025-10-14 19:13:16 -0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdnjs.cloudflare.com",
+      },
+    ],
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
